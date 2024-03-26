d5139b359355867845916c3fbd195c20bb1c90ba
eduardodea85 <eduardodea@outlook.com>
2024-03-26 00:23:03 -0300

    Criando repository e service para Categoria

--- src/main/java/dea/cursomc/resources/CategoriaResource.java
+++ src/main/java/dea/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,31 @@ package dea.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import dea.cursomc.domain.Categoria;
+import dea.cursomc.services.CategoriaService;
 
 @RestController //anotação indica que é uma classa que usa o controlador REST
 @RequestMapping(value="/categorias") //A classe responde pelo caminho do endpoint REST
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
+	
 
 	//Criar método básico
-	@RequestMapping(method=RequestMethod.GET) //Para ser uma função REST, precisa associar com uma das funções do http.
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-		
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //Para ser uma função REST, precisa associar com uma das funções do http.
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+				
 	}
 	
 	
