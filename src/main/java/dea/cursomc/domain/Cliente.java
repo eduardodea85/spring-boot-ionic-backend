2a7093f731aebef4e598df04dab0695f2367adaa
eduardodea85 <eduardodea@outlook.com>
2024-09-16 17:18:57 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/dea/cursomc/domain/Cliente.java
+++ src/main/java/dea/cursomc/domain/Cliente.java
@@ -52,7 +52,7 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo==null) ? null : tipo.getCod(); //Se o tipo for nulo, vou atribuir nulo, caso contrário atribuo tipo.getCod.
 	}
 
 	public Integer getId() {
--- src/main/java/dea/cursomc/services/ClienteService.java
+++ src/main/java/dea/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package dea.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import dea.cursomc.domain.Cliente;
+import dea.cursomc.dto.ClienteDTO;
 import dea.cursomc.repositories.ClienteRepository;
+import dea.cursomc.services.exceptions.DataIntegrityException;
 import dea.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -19,6 +27,44 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Cliente não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	// Os metodos insert e update são iguais, porem quando é para inserir, o objeto
+	// verifica se o id é null. Se não for ele não cria. No caso do update ele
+	// atualiza e verifica se o id já existe com o metodo find.
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
