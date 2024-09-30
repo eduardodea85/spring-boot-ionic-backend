541e882f1e1a91c9b13b9add9d6dd216302d98d7
eduardodea85 <eduardodea@outlook.com>
2024-09-30 11:18:20 -0300

    Validacao customizada: email não repetido na insercao de cliente

--- src/main/java/dea/cursomc/repositories/ClienteRepository.java
+++ src/main/java/dea/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,13 @@
 package dea.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import dea.cursomc.domain.Cliente;
 
 //@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {	
 	
-	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/dea/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/dea/cursomc/services/validation/ClienteInsertValidator.java
@@ -3,8 +3,12 @@ package dea.cursomc.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import dea.cursomc.domain.Cliente;
 import dea.cursomc.domain.enums.TipoCliente;
 import dea.cursomc.dto.ClienteNewDTO;
+import dea.cursomc.repositories.ClienteRepository;
 import dea.cursomc.resources.exception.FieldMessage;
 import dea.cursomc.services.validation.utils.BR;
 import jakarta.validation.ConstraintValidator;
@@ -12,6 +16,10 @@ import jakarta.validation.ConstraintValidatorContext;
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		// inclua os testes aqui, inserindo erros na lista
