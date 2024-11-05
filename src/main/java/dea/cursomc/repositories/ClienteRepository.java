b3ef284ddc31fcfad4db3220f94da258d80ba131
eduardodea85 <eduardodea@outlook.com>
2024-11-04 22:03:29 -0300

    Busca de produtos por nome e categorias

--- src/main/java/dea/cursomc/repositories/ClienteRepository.java
+++ src/main/java/dea/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,12 @@
 package dea.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import dea.cursomc.domain.Cliente;
 
-//@Repository
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {	
 	
 	@Transactional(readOnly = true)
